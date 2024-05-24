cc4644aabcd3a0da2f2592292c7fbf71560150c7
AlejandroZBarajas <falejandrozb@gmail.com>
2024-05-24 16:58:09 -0600

    First commit

---
+++ src/components/organisms/SectionForm.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import Swal from "sweetalert2";
+import Field from "../molecules/Field";
+import Button from "../atoms/Button";
+import './SectionLogin.css'
+
+function SectionForm() {
+    const [nombreAlumno, setnombreAlumnos] = useState('')
+    const [apellidos, setapellidos] = useState('')
+    const [email, setemail]= useState ('')
+    const [fechaNacimiento, setfechaNacimiento] = useState('')
+    const [tutor, settutor] = useState('')
+    const [numeroTutor, setnumeroTutor] = useState ('')
+
+
+    const handlerClick = (event) => {
+        Swal.fire({
+            title: "Exito!",
+            text:  "Alumno creado",
+            footer: ` ${nombreAlumno}  ${apellidos}`,
+            imageUrl: "https://www.svgrepo.com/show/493719/react-javascript-js-framework-facebook.svg",
+            imageHeight: 250,
+            imageAlt: "React Image"
+        });
+    }
+
+    return (
+        <div id="login_section">
+            <Field type="text" placeholder="ej. Daniela" text="Nombre del Alumno (a)" val= {nombreAlumno} fnVal={setnombreAlumnos}/>
+            <Field type="text" placeholder="ej. De la Cruz Jimenez" text="Apellidos" val= {apellidos} fnVal={setapellidos}/>
+            <Field type="email" placeholder="ejemplo@gmail.com" text="email" val={email} fnVal={setemail}/>
+            <Field type="date" placeholder="16/07/2024" text="Fecha de nacimiento" val= {fechaNacimiento} fnVal={setfechaNacimiento}/>
+            <Field type="text" placeholder="ej. Antonio de la Cruz" text="Nombre del tutor (a)" val={tutor} fnVal={settutor}/>
+            <Field type="number" placeholder="ej. 9612767861" text="Numero telefónicp del tutor" val={numeroTutor} fnVal={setnumeroTutor}/>
+
+            <Button type="submit" title="Crear Alumno" onclick={handlerClick}/>
+        </div> 
+     );
+}
+
+export default SectionForm;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import SectionForm from "../components/organisms/SectionForm";
+import './Home.css'
+function Home() {
+    return ( 
+        <div id="login-home">
+            <SectionForm/>
+        </div>
+     );
+}
+
+export default Home;
